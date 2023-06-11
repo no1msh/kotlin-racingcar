e78b865a3b11283ec9cd99ffef0c6d7ceaa6fb6d
Bandal <no1msh1217@gmail.com>
2023-06-11 15:06:34 +0900

    feat: 자동차의 이름은 5글자를 넘지 않는다

--- src/main/kotlin/domain/model/Car.kt
+++ src/main/kotlin/domain/model/Car.kt
@@ -2,4 +2,12 @@ package domain.model
 
 data class Car(
     val name: String
-)
+) {
+    init {
+        require(name.length <= MAX_NAME_LENGTH) { "[잘못된 이름: $name] 자동차의 이름은 5글자를 넘지 않아야합니다." }
+    }
+
+    companion object {
+        private const val MAX_NAME_LENGTH = 5
+    }
+}
--- src/test/kotlin/domain/model/CarTest.kt
+++ src/test/kotlin/domain/model/CarTest.kt
@@ -2,17 +2,30 @@ package domain.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class CarTest {
     @Test
     fun `자동차는 이름을 가진다`() {
         // given
-        val carName = "name"
+        val carName = "name1"
 
         // when
         val car = Car(carName)
 
         // then
         assertThat(car.name).isEqualTo(carName)
+    }
+
+    @Test
+    fun `자동차의 이름은 5글자를 넘지 않는다`() {
+        // given
+        val carName = "sixLen"
+
+        // when
+        val errorMessage = assertThrows<IllegalArgumentException> { Car(carName) }.message
+
+        // then
+        assertThat(errorMessage).isEqualTo("[잘못된 이름: $carName] 자동차의 이름은 5글자를 넘지 않아야합니다.")
     }
 }
