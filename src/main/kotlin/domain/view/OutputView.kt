e598356b263e2c50d90b7e21121f95f0c50cab4d
Bandal <no1msh1217@gmail.com>
2023-06-11 21:34:05 +0900

    feat: 출력 기능 구현
    - 경주할 자동차 이름들을 요청 출력 기능
    - 트랙을 몇바퀴 돌건지 요청 출력 기능
    - 실행결과를 출력
    - 각 자동차들의 진행상황을 출력
    - 우승자들을 출력

---
+++ src/main/kotlin/domain/view/OutputView.kt
@@ -0,0 +1,32 @@
+package domain.view
+
+import domain.model.Car
+import domain.model.CarName
+
+object OutputView {
+    private const val WINNER_NAMES_SEPARATOR = ", "
+    private const val POSITION_DISPLAY = "-"
+
+    fun printRequestCarNames() {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+    }
+
+    fun printRequestLapTimes() {
+        println("시도할 횟수는 몇 회인가요?")
+    }
+
+    fun printRacingResult() {
+        println("실행 결과")
+    }
+
+    fun printCurrentProgress(cars: List<Car>) {
+        cars.forEach {
+            println("${it.name} : ${POSITION_DISPLAY.repeat(it.position)}")
+        }
+        println()
+    }
+
+    fun printWinners(winnerNames: List<CarName>) {
+        println("최종 우승자: ${winnerNames.joinToString(WINNER_NAMES_SEPARATOR)}")
+    }
+}
